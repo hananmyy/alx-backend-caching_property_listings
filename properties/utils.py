0fb2fa682ce00306f711ef07a2deccc96a158fbd
Hanan Mohamud <hananmohamudy@gmail.com>
2025-07-13 16:12:41 +0300

    updated files

--- properties/utils.py
+++ properties/utils.py
@@ -1,20 +1,35 @@
 from django_redis import get_redis_connection
 import logging
+from django.core.cache import cache
+from properties.models import Property
+
+
+def get_all_properties():
+    all_properties = cache.get('all_properties')
+    if all_properties is None:
+        all_properties = list(Property.objects.values('title', 'description', 'price', 'location', 'created_at'))
+        cache.set('all_properties', all_properties, 3600)
+    return all_properties
+
+
+
+logger = logging.getLogger(__name__)
 
 def get_redis_cache_metrics():
     redis_conn = get_redis_connection("default")
     info = redis_conn.info()
 
-    hits = info.get("keyspace_hits", 0)
-    misses = info.get("keyspace_misses", 0)
-    total = hits + misses
-    hit_ratio = (hits / total) if total > 0 else 0
+    keyspace_hits = info.get("keyspace_hits", 0)
+    keyspace_misses = info.get("keyspace_misses", 0)
+    total_requests = keyspace_hits + keyspace_misses
+
+    hit_ratio = keyspace_hits / total_requests if total_requests > 0 else 0 
 
     metrics = {
-        "hits": hits,
-        "misses": misses,
+        "hits": keyspace_hits,
+        "misses": keyspace_misses,
         "hit_ratio": round(hit_ratio, 3)
     }
 
-    logging.info(f"Redis Cache Metrics: {metrics}")
+    logger.error(f"Redis Cache Metrics: {metrics}")
     return metrics
--- properties/views.py
+++ properties/views.py
@@ -2,12 +2,20 @@ from django.views.decorators.cache import cache_page
 from django.shortcuts import render
 from .models import Property
 from properties.utils import get_redis_cache_metrics
+from django.http import JsonResponse
+from .utils import get_all_properties
 
 
-@cache_page(60 * 15)  # Cache for 15 minutes
+"""@cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all()
-    return render(request, 'property_list.html', {'properties': properties})
+    return render(request, 'property_list.html', {'properties': properties})"""
+
+def property_list(request):
+    properties = get_all_properties()
+
+    return JsonResponse({'data': properties})
+
 
 
 print(get_redis_cache_metrics())
