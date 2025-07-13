a95d0ab48be8441525f820cb2afc5c35785826fc
Hanan Mohamud <hananmohamudy@gmail.com>
2025-07-13 16:15:39 +0300

    updated files

--- properties/utils.py
+++ properties/utils.py
@@ -8,8 +8,8 @@ from properties.models import Property
 def getallproperties():
     all_properties = cache.get('all_properties')
     if all_properties is None:
-        all_properties = list(Property.objects.values('title', 'description', 'price', 'location', 'created_at'))  # ✅ includes queryset
-        cache.set('all_properties', all_properties, 3600)  # cache for 1 hour
+        all_properties = list(Property.objects.all())
+        cache.set('all_properties', all_properties, 3600)
     return all_properties
 
 
